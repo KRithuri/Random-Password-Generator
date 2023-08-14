a523cd83f325a99c6e164365ed2747eb849c4e0e
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-14 12:43:30 +0200

    code to generate the password

--- generate.py
+++ generate.py
@@ -5,7 +5,19 @@ lower = list(string.ascii_lowercase)
 numbers = list(string.digits)
 special = list(string.punctuation)
 upper = list(string.ascii_uppercase)
+listAll = lower+numbers+special+upper 
 
 password = []
 
 combination = ""
+
+n = int(input("How many passwords do you want to generate: "))
+length = int(input("Enter length of password: "))
+
+for i in range(n):
+    combination = ""
+    for j in range(length):
+        combination += str(random.choice(listAll))
+    password.append(combination)
+
+print(password)
