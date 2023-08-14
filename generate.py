790ff749c34f610b7ebfa921a8caf4c5b4653d75
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-14 12:44:22 +0200

    put code inside function

--- generate.py
+++ generate.py
@@ -1,23 +1,24 @@
-import random
-import string 
+def password_generator(n,length):
+    import random
+    import string 
 
-lower = list(string.ascii_lowercase)
-numbers = list(string.digits)
-special = list(string.punctuation)
-upper = list(string.ascii_uppercase)
-listAll = lower+numbers+special+upper 
+    lower = list(string.ascii_lowercase)
+    numbers = list(string.digits)
+    special = list(string.punctuation)
+    upper = list(string.ascii_uppercase)
+    listAll = lower+numbers+special+upper 
 
-password = []
+    password = []
 
-combination = ""
-
-n = int(input("How many passwords do you want to generate: "))
-length = int(input("Enter length of password: "))
-
-for i in range(n):
     combination = ""
-    for j in range(length):
-        combination += str(random.choice(listAll))
-    password.append(combination)
 
-print(password)
+    n = int(input("How many passwords do you want to generate: "))
+    length = int(input("Enter length of password: "))
+
+    for i in range(n):
+        combination = ""
+        for j in range(length):
+            combination += str(random.choice(listAll))
+        password.append(combination)
+
+    print(password)
